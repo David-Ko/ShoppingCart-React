6cdc79f9d0809950f065ecef3d19d0234edb624b
David Ko <dkoflow@gmail.com>
2019-03-02 16:26:59 -0800

    Added bootstrap and created a landing page

---
+++ src/components/Counter.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+
+class Counter extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <Button>Hit Me</Button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
