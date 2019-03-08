0ff3d6486e76bfcebd2498d4afba31ae6d022fb0
David Ko <dkoflow@gmail.com>
2019-03-08 10:01:17 -0800

    Modified NavBar name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ class NavBar extends Component {
     return (
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
-          Navbar
+          Shopping Cart Template
         </a>
       </nav>
     );
